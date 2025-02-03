2f714b90171e5c015e5fb8670dab9df8b270a6de
FuriousNayan <npatel24@my.waketech.edu>
2025-02-03 12:42:46 -0500

    Pushing half of the work

---
+++ js03.js
@@ -0,0 +1,65 @@
+/*    JavaScript 7th Edition
+     Chapter 3
+     Chapter case
+
+     Tipton Turbines
+     Program to display games results in a web table
+     Author: 
+     Date:   
+
+     Filename: js03.js
+ */
+
+// Days of the week
+let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+window.addEventListener("load", addWeekDays);
+
+// Function to write weekday names into the calendar
+function addWeekDays(){
+    let i = 0; // inital counter value
+
+    // reference the collection of heading cells
+    let headingCells = document.getElementsByTagName("th");
+
+    // write each of the seven days into a heading cell
+    while (i < 7){
+        headingCells[i].innerHTML = weekDays[i];
+
+        //Increase the counter by 1
+        i++;
+    }
+}
+
+window.addEventListener("load", showGames);
+
+// Function to write game information into the calendar
+function showGames() {
+    for (let i = 0; i < gameDates.length; i++) {
+        let gameInfo = "";
+
+        //Open the paragraph
+        gameInfo += "<p>";
+
+        // Display the game location
+        if (gameLocations[i] === "h"){
+            gameInfo += "vs. ";
+            
+        } else if (gameLocations[i] === "a"){
+            gameInfo += "@ ";
+        }
+
+        // Include the opponent
+        gameInfo += gameOpponents[i] + "<br>";
+
+        // Include the result and score
+        gameInfo += gameResults[i] + ": (" + runsScored[i] + "-" + runsAllowed[i] + ")";
+
+        // Close the paragraph
+        gameInfo += "</p>";
+
+        // Write the information into a table cell
+        let tableCell = document.getElementById(gameDates[i]);
+        tableCell.insertAdjacentHTML("beforeEnd", gameInfo);
+    }
+}
